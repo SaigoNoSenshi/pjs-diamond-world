02c13cd611a3a297d1d515b74d6d666d4984d578
John Paul Betis <56253973+SaigoNoSenshi@users.noreply.github.com>
2026-09-20 22:36:57 +0800

    Import src/types/assets.d.ts

---
+++ src/types/assets.d.ts
@@ -0,0 +1,20 @@
+declare module '*.png' {
+  const value: number;
+  export default value;
+}
+declare module '*.jpg' {
+  const value: number;
+  export default value;
+}
+declare module '*.webp' {
+  const value: number;
+  export default value;
+}
+declare module '*.mp3' {
+  const value: number;
+  export default value;
+}
+declare module '*.wav' {
+  const value: number;
+  export default value;
+}
